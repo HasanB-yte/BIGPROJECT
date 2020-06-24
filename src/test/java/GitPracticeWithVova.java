7da3d538c39a9003aa574b13aad17a05f592a161
Silond <silond@gmail.com>
2020-06-24 13:16:35 -0500

    how and why

--- src/test/java/GitPracticeWithVova.java
+++ src/test/java/GitPracticeWithVova.java
@@ -5,6 +5,14 @@ public class GitPracticeWithVova {
         System.out.println("Hello World");
         System.out.println("Hello Roman and Vova");
         System.out.println("Hello World");
+
+        System.out.println("Hello World");
+        System.out.println("Hello Roman and Vova");
+        System.out.println("Hello World");
+
+        System.out.println("Hello World");
+        System.out.println("Hello Roman and Vova");
+        System.out.println("Hello World");
      
 
         
--- src/test/java/PracticeGit.java
+++ src/test/java/PracticeGit.java
@@ -1,3 +1,5 @@
 public class PracticeGit {
   System.out.println("Hello World again");
+  System.out.println("Hello World again "+1);
+  System.out.println("Hello World again " +2);
 }
