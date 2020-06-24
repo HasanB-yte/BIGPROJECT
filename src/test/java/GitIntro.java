4d05b6259a1d6f5e41710f6318ae35efdf0eb02a
Silond <silond@gmail.com>
2020-06-24 13:58:52 -0500

    back to original version

--- src/test/java/GitIntro.java
+++ src/test/java/GitIntro.java
@@ -11,14 +11,6 @@ public class GitIntro {
         StringBuilder reversed1= new StringBuilder();
         reversed1.append(reversed).reverse();
         System.out.println(reversed1);
-        System.out.println(reversed1);
-        System.out.println(reversed1);
-        
-        System.out.println("yes we did it");
-        System.out.println("yes we did it");
-        System.out.println("yes we did it");
-        System.out.println("yes we did it");
-        System.out.println("yes we did it");
 
         
     }
--- src/test/java/PracticeGit.java
+++ src/test/java/PracticeGit.java
@@ -1,13 +1,4 @@
 public class PracticeGit {
   System.out.println("Hello World again");
-  System.out.println("Hello World again "+1);
-  System.out.println("Hello World again "+2);
-  
-  System.out.println("yes we did it");
-   System.out.println("yes we did it");
-    System.out.println("yes we did it");
-     System.out.println("yes we did it");
-      System.out.println("yes we did it");
-       System.out.println("yes we did it");
-       System.out.println("yes we did it")
+
 }
